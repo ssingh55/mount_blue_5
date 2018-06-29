dfc653aa5571dd68a941a6d50be7da93b096e181
Shubham Singh <shubham.singh@mountblue.io>
2018-06-29 19:19:50 +0530

    king moving perfectly

--- dragndrop.js
+++ dragndrop.js
@@ -1,26 +1,55 @@
-var currEleId="";
-var color="";
+var currEleId = "";
+var color = "";
+var piece_name = "";
 
-function allowDrop(ev){
-	ev.preventDefault();
+function allowDrop(ev) {
+    ev.preventDefault();
 }
 
-function registerTheElementId(ev){
-	 console.log(ev);
-	ev.dataTransfer.setData('text', ev.target.id);
-	currEleId=ev.path;
-	color=currEleId[0].classList[0];
-	currEleId=currEleId[1].id;
-	// console.log(currEleId);
+function registerTheElementId(ev) {
+    console.log(ev);
+    ev.dataTransfer.setData('text', ev.target.id);
+    currEleId = ev.path;
+    piece_name = currEleId[0].id.substring(6);
+    console.log(piece_name);
+    color = currEleId[0].classList[0];
+    currEleId = currEleId[1].id;
+    // console.log(currEleId);
 }
 
-function addTheElement(ev){
-	// console.log(ev);
-	//check for pawn
-	var diff=Number((currEleId+"").substring(1))-Number(ev.toElement.id.substring(1));
-	if(ev.toElement.id==isValidMovePawn(currEleId,color,diff)){
-		var eleId = ev.dataTransfer.getData('text');
-		var ele = document.getElementById(eleId);
-		ev.target.appendChild(ele);
-	}
+function addTheElement(ev) {
+    // console.log(ev);
+    if (piece_name.substring(0, 4) == "pawn") piece_name = "pawn";
+    else if (piece_name.substring(0, 6) == "knight") piece_name = "knight";
+    else if (piece_name.substring(0, 4) == "rook") piece_name = "rook";
+    else if (piece_name.substring(0, 6) == "bishop") piece_name = "bishop";
+
+    switch (piece_name) {
+        //check for pawn
+        case "king":
+        	var diff = Number((currEleId + "").substring(1)) - Number(ev.toElement.id.substring(1));
+        	console.log(diff);
+        	if (isValidMoveKing(ev.toElement.id,currEleId, color,diff)) {
+                var eleId = ev.dataTransfer.getData('text');
+                var ele = document.getElementById(eleId);
+                ev.target.appendChild(ele);
+            }
+        case "bishop":
+
+        case "rook":
+
+        case "knight":
+
+
+        case "queen":
+            break;
+        case "pawn":
+            var diff = Number((currEleId + "").substring(1)) - Number(ev.toElement.id.substring(1));
+            if (isValidMovePawn(ev.toElement.id,currEleId, color, diff)) {
+                var eleId = ev.dataTransfer.getData('text');
+                var ele = document.getElementById(eleId);
+                ev.target.appendChild(ele);
+            }
+            break;
+    }
 }
--- validmove.js
+++ validmove.js
@@ -1,15 +1,39 @@
-function isValidMovePawn(currEleId,color,diff){
-	// console.log(diff);
-	if (color=='black'){
-		if(Number((currEleId+"").substring(1))==7&&diff==2)
-			return (currEleId+"").substring(0,1)+""+(Number((currEleId+"").substring(1))-2);
-		else
-			return (currEleId+"").substring(0,1)+""+(Number((currEleId+"").substring(1))-1);
+function isValidMovePawn(evEleId,currEleId, color, diff) {
+    if (color == 'black') {
+        if (Number((currEleId + "").substring(1)) == 7 && diff == 2)
+            return evEleId==(currEleId + "").substring(0, 1) + "" + (Number((currEleId + "").substring(1)) - 2);
+        else
+            return evEleId==(currEleId + "").substring(0, 1) + "" + (Number((currEleId + "").substring(1)) - 1);
+    } else if (color == 'white') {
+        if (Number((currEleId + "").substring(1)) == 2 && diff == -2)
+            return evEleId==(currEleId + "").substring(0, 1) + "" + (Number((currEleId + "").substring(1)) + 2);
+        else
+            return evEleId==(currEleId + "").substring(0, 1) + "" + (Number((currEleId + "").substring(1)) + 1);
+    }
+}
+
+function isValidMoveKing(evEleId,currenEleId,color,diff){
+	if(color=='black'){
+		if(diff==1)
+		{
+			return true;
+		}
+		else if(diff==-1)
+		{
+			return true;
+		}
+		else if(diff==0)
+		{
+			if(String.fromCharCode(evEleId.charCodeAt() + 1)==currenEleId.substring(0,1))
+				return true;
+			else if(String.fromCharCode(evEleId.charCodeAt() - 1)==currenEleId.substring(0,1))
+				return true;
+			else
+				return false;
+		}
 	}
-	else if(color=='white'){
-		if(Number((currEleId+"").substring(1))==2&&diff==-2)
-			return (currEleId+"").substring(0,1)+""+(Number((currEleId+"").substring(1))+2);
-		else
-			return (currEleId+"").substring(0,1)+""+(Number((currEleId+"").substring(1))+1);
+	else if(color = 'white')
+	{
+
 	}
 }
